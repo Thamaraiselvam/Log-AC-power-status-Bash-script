e2759337063424fd7169cd8e5daf1426580f95c7
Thamaraiselvam <thamaraiselvam@live.com>
2017-06-16 13:28:26 +0530

    Update battery-logger.sh
    update with percentage

--- battery-logger.sh
+++ battery-logger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if on_ac_power; then 
-    echo "plugged in $(date)" >> /path/to/battery-data.txt ## Laptop on power
+    echo "plugged in : $(acpi) and time :  $(date)" >> /path/to/battery-data.txt ## Laptop on power
 else
-    echo "plugin out $(date)" >> /path/to/battery-data.txt ## Laptop on battery
+    echo "plugged out : $(acpi) and time :  $(date)" >> /path/to/battery-data.txt ## Laptop on battery
 fi
