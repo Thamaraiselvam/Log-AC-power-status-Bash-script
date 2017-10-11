f60601388414f9f051999e3a0a91dc85c4137d29
Thamaraiselvam <thamaraiselvam@live.com>
2017-10-11 16:30:49 +0530

    Update battery-logger.sh

--- battery-logger.sh
+++ battery-logger.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if on_ac_power; then 
-    echo "plugged in : $(acpi) and time :  $(date)" >> /path/to/battery-data.txt ## Connected
-else
-    echo "plugged out : $(acpi) and time :  $(date)" >> /path/to/battery-data.txt ## Not connected
-fi
+echo "$(acpi) and time :  $(date)" >> /var/log/battery-history/battery-data.txt ## Connected
